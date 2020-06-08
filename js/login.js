f6e9144e14a1d7cc684d3985e0852c86f776be65
Krue1 <879924250@qq.com>
2020-06-08 11:40:58 +0800

    上线注册和登录功能

---
+++ js/login.js
@@ -0,0 +1,59 @@
+let account = document.querySelector("#account");
+let password = document.querySelector("#password");
+
+function checkAccount() {
+  if (account.validity.patternMismatch) {
+    account.setCustomValidity("帐号必须为6-16位的数字和字母的组合");
+  } else account.setCustomValidity("");
+}
+function checkPassword() {
+  if (password.validity.patternMismatch) {
+    password.setCustomValidity("密码必须为6-16位的数字和字母的组合");
+  } else password.setCustomValidity("");
+}
+account.addEventListener("blur", checkAccount);
+password.addEventListener("blur", checkPassword);
+
+$("#login-form").on("submit", function (e) {
+  e.preventDefault();
+  let userName = account.value;
+  let passWord = password.value;
+  console.log("username:" + userName);
+  console.log("password:" + passWord);
+
+  $.ajax({
+    type: "GET",
+    dataType: "json",
+    url: "http://47.100.62.222:80/RSAPublicKey/" + userName,
+    success: function (result) {
+      console.log("加密返回码 :" + result.code);
+      if (result.code == 00000) {
+        let publicKey = result.data.publicKey;
+        let encrypt = new JSEncrypt();
+        encrypt.setPublicKey(publicKey);
+        let encryptedPassword = encrypt.encrypt(passWord);
+
+        $.ajax({
+          type: "POST",
+          dataType: "json",
+          url: "http://47.100.62.222:80/login",
+          contentType: "application/json",
+          data: JSON.stringify({
+            username: userName,
+            password: encryptedPassword,
+          }),
+          success: function (result) {
+            if (result.code == 00000) {
+              alert("登录成功");
+              window.location.href = "homepage.html";
+            } else if (result.code == 10101) {
+              alert("用户名或密码不符合要求");
+            } else if (result.code == 10201) {
+              alert("帐号或密码错误");
+            }
+          },
+        });
+      }
+    },
+  });
+});
---
+++ js/signup.js
@@ -0,0 +1,73 @@
+let account = document.querySelector("#account");
+let passwd1 = document.querySelector("#password1");
+let passwd2 = document.querySelector("#password2");
+
+function checkAccount() {
+  if (account.validity.patternMismatch) {
+    account.setCustomValidity("帐号必须为6-16位的数字和字母的组合");
+  } else account.setCustomValidity("");
+}
+function validate() {
+  if (passwd2.validity.patternMismatch) {
+    passwd2.setCustomValidity("密码必须为6-16位的数字和字母的组合");
+  } else passwd2.setCustomValidity("");
+  if (passwd1.value != passwd2.value) {
+    passwd2.setCustomValidity("两次输入的密码不一致");
+  } else passwd2.setCustomValidity("");
+}
+function checkPassword() {
+  if (passwd1.validity.patternMismatch) {
+    passwd1.setCustomValidity("密码必须为6-16位的数字和字母的组合");
+  } else passwd1.setCustomValidity("");
+}
+account.addEventListener("blur", checkAccount);
+passwd1.addEventListener("blur", checkPassword);
+passwd2.addEventListener("blur", validate);
+
+let signupBtn = document.querySelector("#signup-btn");
+
+$("#signup-form").on("submit", function (e) {
+  e.preventDefault();
+  let userName = account.value;
+  let passWord = passwd2.value;
+  console.log("username:" + userName);
+  console.log("password:" + passWord);
+
+  $.ajax({
+    type: "GET",
+    dataType: "json",
+    url: "http://47.100.62.222:80/RSAPublicKey/" + userName,
+    success: function (result) {
+      console.log("加密返回码 :" + result.code);
+      if (result.code == 00000) {
+        let publicKey = result.data.publicKey;
+        let encrypt = new JSEncrypt();
+        encrypt.setPublicKey(publicKey);
+        let encryptedPassword = encrypt.encrypt(passWord);
+
+        $.ajax({
+          type: "POST",
+          dataType: "json",
+          url: "http://47.100.62.222:80/register",
+          contentType: "application/json",
+          data: JSON.stringify({
+            username: userName,
+            password: encryptedPassword,
+          }),
+          success: function (result) {
+            if (result.code == 00000) {
+              alert("注册成功");
+              window.location.href = "login.html";
+            } else if (result.code == 10101) {
+              alert("用户名或密码不符合要求");
+            } else if (result.code == 10303) {
+              alert("帐号已被注册");
+            } else if (result.code == 10302) {
+              alert("注册失败");
+            }
+          },
+        });
+      }
+    },
+  });
+});
